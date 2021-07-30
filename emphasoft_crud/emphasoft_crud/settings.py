516cbc305c61224abf61635e58ad15e681b38587
IAEvtushenko <kurlzz863@gmail.com>
2021-07-30 20:34:20 +0300

    Changed allowed host

--- emphasoft_crud/emphasoft_crud/settings.py
+++ emphasoft_crud/emphasoft_crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-+9vdhhhq5&ch1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['emphasofttestcrud.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
