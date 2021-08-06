2bd14f560401f184753e60ae603de4e754094e06
IAEvtushenko <kurlzz863@gmail.com>
2021-08-06 13:19:38 +0300

    Api urls changed

--- emphasoft_crud/api/urls.py
+++ emphasoft_crud/api/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('create/notes/', NoteCreateView.as_view()),
     path('notes/', NoteListView.as_view()),
     path('notes/<int:pk>/', NoteDetailView.as_view()),
-    path('create_user/', UserCreateView.as_view()),
     path('users/', UserListView.as_view()),
     path('users/<int:pk>/', UserDetailView.as_view()),
 ]
